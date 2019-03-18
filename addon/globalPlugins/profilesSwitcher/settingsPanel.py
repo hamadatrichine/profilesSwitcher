3c1f45863aacfed5a8a53e19a730d42e0ce09dfc
hamada trichine <35621856+hamadatrichine@users.noreply.github.com>
2019-03-18 17:59:12 +0100

    fixed translation error

--- addon/globalPlugins/profilesSwitcher/settingsPanel.py
+++ addon/globalPlugins/profilesSwitcher/settingsPanel.py
@@ -17,14 +17,21 @@ class SettingsPanel(gui.SettingsPanel):
         profilesList.insert(0, transFromNVDA('(normal configuration)'))
         self.firstProfile = boxSizer.addLabeledControl(
             # Translators: The label of the first profile selection widget.
-            _("First profile to switch to:"),
+            _("&First profile to switch to:"),
             wx.Choice, choices=profilesList
         )
         self.secondProfile = boxSizer.addLabeledControl(
             # Translators: The label of the secondary profile selection widget.
-            _("Second profile to switch to:"),
+            _("&Second profile to switch to:"),
             wx.Choice, choices=profilesList
         )
+        self.playSound = boxSizer.addItem(wx.CheckBox(
+            self,
+            id=-1,
+            # Translators: The checkBox label of turning sound playing on alerts on or off.
+            label=_('&Play sound on alerts.'))
+        )
+        self.playSound.SetValue(config.conf['profilesSwitcher']['shouldPlaySound'])
         firstChoice = self.firstProfile.FindString(config.conf['profilesSwitcher']['firstProfile'])
         secondChoice = self.secondProfile.FindString(config.conf['profilesSwitcher']['secondProfile'])
         self.firstProfile.SetSelection(firstChoice if firstChoice != wx.NOT_FOUND else 0)
@@ -35,4 +42,5 @@ class SettingsPanel(gui.SettingsPanel):
         secondChoice = self.secondProfile.GetSelection()
         config.conf['profilesSwitcher']['firstProfile'] = self.firstProfile.GetString(firstChoice) if firstChoice else ''
         config.conf['profilesSwitcher']['secondProfile'] = self.secondProfile.GetString(secondChoice) if secondChoice else ''
+        config.conf['profilesSwitcher']['shouldPlaySound'] = self.playSound.GetValue()
 
